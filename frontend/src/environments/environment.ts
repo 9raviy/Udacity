b3595a29067fc3902affd6a874e7d3dc07aab005
ravi <9raviy@iima.ac.in>
2020-06-25 23:23:47 -0400

    final submission

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,15 @@
+/* @TODO replace with your variables
+ * ensure all variables on this page match your project
+ */
+
+export const environment = {
+  production: false,
+  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  auth0: {
+    url: 'loginpath.us', // the auth0 domain prefix
+    audience: 'http://localhost:5000', // the audience set for the auth0 app
+    clientId: 'KUIxyBlfi4N4gg6k7oYyzLDUqb7P8A7o', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+  }
+};
+
